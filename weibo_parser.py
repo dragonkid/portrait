0f307f5d33df8d8e0527df40d8ce6a59e3524f2b
dragonkid <idragonkid@gmail.com>
2018-07-14 16:07:20 +0800

    tmp

--- weibo_parser.py
+++ weibo_parser.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import copy
+
+
 def extract_personal_infos():
     """
     extract weibo personal infos from xlsx to txt
@@ -13,38 +16,86 @@ def extract_personal_infos():
             f.write(v.value.strip() + '\n')
 
 
-TAGS = {
-    '基本信息': {
-        '姓名',
-        '血型',
-        '昵称',
-        '所在地',
-        '性别',
-        '感情状况',
-        '简介',
-        '生日',
-        '注册时间',
-        '个性域名',
-        '博客',
-        '取向',
+info_structure = {
+    'basic': {              # 基本信息
+        'name': '',         # 姓名
+        'blood_group': '',  # 血型
+        'nickname': '',     # 昵称
+        'location': '',     # 所在地
+        'sex': '',          # 性别
+        'relationship_status': '',  # 感情状况
+        'profile': '',      # 简介
+        'birthday': '',     # 生日
+        'registration': '', # 注册时间
+        'domain': '',       # 个性域名
+        'blog': '',         # 博客
+        'sexual_orientation': '',   # 性取向
+        'raw': ''
     },
-    '标签信息':{
-        '标签',
+    'tags': {               # 标签信息
+        'segmentation': [],
+        'raw': ''
     },
-    '联系信息': {
-        '邮箱',
+    'contacts': {           # 联系信息
+        'email': '',        # 邮箱
+        'raw': ''
     },
-    '工作信息': {
-        '公司',
-        '地区',
-        '职位',
+    'job': {                # 工作信息
+        'company': '',      # 公司
+        'location': '',     # 地区
+        'title': '',        # 职位
+        'raw': ''
     },
-    '教育信息': {
-        '大学',
-        '高中',
-        '初中',
-        '小学',
-        '技校',
+    'education': {          # 教育信息
+        'university': '',   # 大学
+        'senior_high_school': '',   # 高中
+        'junior_high_school': '',   # 初中
+        'primary_school': '',       # 小学
+        'technical_school': '',     # 技校
+        'raw': ''
+    }
+}
+
+
+roots = {
+    '基本信息': 'basic',
+    '标签信息': 'tags',
+    '联系信息': 'contacts',
+    '工作信息': 'job',
+    '教育信息': 'education'
+}
+leafs = {
+    'basic': {
+        '姓名': 'name',
+        '血型': 'blood_group',
+        '昵称': 'nickname',
+        '所在地': 'location',
+        '性别': 'sex',
+        '感情状况': 'relationship_status',
+        '简介': 'profile',
+        '生日': 'birthday',
+        '注册时间': 'registration',
+        '个性域名': 'domain',
+        '博客': 'blog',
+        '性取向': 'sexual_orientation',
+    },
+    'tags': {
+        '标签': 'segmentation'
+    },
+    'contacts': {
+        '邮箱': 'email',
+    },
+    'job': {
+        '公司': 'company',
+        '地区': 'location',
+        '职位': 'title',
+    },
+    'education': {
+        '大学': 'university',
+        '高中': 'senior_high_school',
+        '初中': 'junior_high_school',
+        '小学': 'primary_school',
+        '技校': 'technical_school'
     }
 }
 
@@ -52,7 +103,16 @@ TAGS = {
 def unpack_info(line):
     """
     unpack txt to dict
+
+    >>>info = unpack_info('基本信息昵称：Ybwy0923所在地：四川 宜宾标签信息标签：化妆造型瘦身减肥')
+    ...info['basic']['nickname'] == 'Ybwy0923'
+    ...info['basic']['location'] == '四川 宜宾'
+    ...info['tags']['raw'] == '化妆造型瘦身减肥'
+    True
+    True
+    True
     """
+    # info = unpack_root(line)
     pass
 
 
